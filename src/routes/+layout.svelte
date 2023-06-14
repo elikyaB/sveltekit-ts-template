ce64a08a7de6e589ac7ea93ee46ead7910cd919b
elikya <elikyabokanga@gmail.com>
2023-06-14 01:02:07 -0400

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Navbar from "../lib/Navbar.svelte";
+    import Footer from "../lib/Footer.svelte";
+    import Notifications from "../lib/Notifications.svelte";
+</script>
+
+<Navbar/>
+<Notifications/>
+<slot/>
+<Footer/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+
+</script>
+
+<main>
+  content here
+</main>
+
+<style lang="scss">
+  main {color: $color;}
+</style>
